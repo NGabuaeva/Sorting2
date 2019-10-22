ea086ace7b1cffbe103dce9fd16eb5f135beefb2
Emma Munley <emmamunley@gmail.com>
2019-10-22 15:09:49 -0400

    test specs

---
+++ mergesort.spec.js
@@ -0,0 +1,28 @@
+describe('Bubble Sort', function() {
+  it('handles an empty array', function() {
+    expect(bubbleSort([])).toEqual([]);
+  });
+  it('handles an array with 1 element', function() {
+    expect(bubbleSort([2])).toEqual([2]);
+  });
+  it('sorts an array of 2 elements', function() {
+    expect(bubbleSort([2, 1])).toEqual([1, 2]);
+  });
+  it('handles negative numbers', function() {
+    expect(bubbleSort([1, -1])).toEqual([-1, 1]);
+  });
+  it('sorts an array of 10 elements', function() {
+    expect(bubbleSort([23, 16, 8, 15, 4, 42, 10, -7, 0, 100])).toEqual([
+      -7,
+      0,
+      4,
+      8,
+      10,
+      15,
+      16,
+      23,
+      42,
+      100,
+    ]);
+  });
+});
